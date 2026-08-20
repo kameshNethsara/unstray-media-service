80d591e339cebbb03e9ac8a53a889257c578897f
kameshNethsara <kamesh317nethsara@gmail.com>
2026-08-20 23:14:17 +0530

    Add initial project structure with Maven configuration and basic test setup

---
+++ src/test/java/com/unstray/services/media_service/MediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unstray.services.media_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
