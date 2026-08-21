4db9ce19de6f8e8d17fbe3fd305cb1fef735f97a
kameshNethsara <kamesh317nethsara@gmail.com>
2026-08-21 16:49:47 +0530

    Add fields to MediaUploadResponse for file metadata and URL

--- src/main/java/com/unstray/services/media_service/dto/MediaUploadResponse.java
+++ src/main/java/com/unstray/services/media_service/dto/MediaUploadResponse.java
@@ -1,4 +1,19 @@
 package com.unstray.services.media_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MediaUploadResponse {
+
+    private String fileName;
+    private String objectName;
+    private String contentType;
+    private Long size;
+    private String url;
 }
