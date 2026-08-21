8f95bc8a6294b0adfe84a49ac87c08a07238fa31
kameshNethsara <kamesh317nethsara@gmail.com>
2026-08-21 16:50:32 +0530

    Remove duplicate import statements in MediaServiceApplication

--- src/main/java/com/unstray/services/media_service/MediaServiceApplication.java
+++ src/main/java/com/unstray/services/media_service/MediaServiceApplication.java
@@ -3,6 +3,9 @@ package com.unstray.services.media_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class MediaServiceApplication {
 
